77a9b4591f8764d9fc8f108eac107d393fcfc1c5
Amanej <aman.mender@tegningtil3d.no>
2016-02-14 00:22:45 +0100

    Fixed users, post creation and some other features

--- client/common.js
+++ client/common.js
@@ -1,6 +1,17 @@
 Meteor.startup(function () {
     AccountsEntry.config({
       homeRoute: '/',                    // mandatory - path to redirect to after sign-out
-      dashboardRoute: '/'      // mandatory - path to redirect to after successful sign-in
+      dashboardRoute: '/',      // mandatory - path to redirect to after successful sign-in7
+      passwordSignupFields: 'USERNAME_AND_EMAIL'
     });
+
+    Accounts.ui.config({
+      passwordSignupFields: 'USERNAME_AND_EMAIL'
+    })
   });
+
+// Global helper/function for retrieving the profile images of unique users
+Template.registerHelper('getProfileImg', function(userId){
+  var imgUrl = UserImages.findOne({userId: userId}).image;
+  return imgUrl;
+});
